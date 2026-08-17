b8c5dc09386b6cf376039c1bc0616e620c4168fe
hananitallyson <tallysonhanani@gmail.com>
2026-08-17 14:35:06 -0300

    add: sum in C and assembly

---
+++ asm/sum.s
@@ -0,0 +1,25 @@
+	.file	"sum.c"
+	.text
+	.globl	sum
+	.type	sum, @function
+sum:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	sum, .-sum
+	.ident	"GCC: (GNU) 16.2.1 20260810"
+	.section	.note.GNU-stack,"",@progbits
