7389a9aa8a830c24f9a44eb33ffe7f18bbd24545
hananitallyson <tallysonhanani@gmail.com>
2026-08-24 13:14:13 -0300

    reset

--- define_pi.c
+++
@@ -1,6 +0,0 @@
-#define PI 3.14159
-
-int main() {
-    printf("%f\n", PI);
-    return 0;
-}
--- main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int sum(int a, int b);
-
-int main(void) {
-    printf("%d", sum(10, 20));
-    return 0;
-}
